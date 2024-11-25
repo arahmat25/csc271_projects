1f37c0033912c8c061b1672ccfd3bea085fc2f9d
arahmat25 <90437529+arahmat25@users.noreply.github.com>
2024-11-24 19:12:56 -0500

    Update loop.js

--- product_site/js/loop.js
+++ product_site/js/loop.js
@@ -1,22 +1,20 @@
-//array to hold images
-var images=[
-    {src: 'images/cambodia.webp', url:'cusine.html' },
-    {src: 'images/malaysia.jfif', url: 'cusine.html'},
-    {src: 'images/Flag-Philippines.webp',url: 'cusine.html'}
+// array to hold images
+var images = [
+    { src: 'images/cambodia.webp', url: 'cusine.html' },
+    { src: 'images/malaysia.jfif', url: 'cusine.html' },
+    { src: 'images/Flag-Philippines.webp', url: 'cusine.html' }
 ];
 
-function redirect(index){
-    //store the index to know which object to display
-    localStorage.setItem('objectIndex',index);
+function redirect(index) {
+    // store the index to know which object to display
+    localStorage.setItem('objectIndex', index);
     window.location.href = images[index].url;
-    
-   
-   
 }
 
 // get elements in imageContainer
 var container = document.getElementById('imageContainer');
 let currentIndex = 0;
+
 // Loop through each image in the array and create img elements
 for (let index = 0; index < images.length; index++) {
     var img = document.createElement('img');
@@ -24,24 +22,24 @@ for (let index = 0; index < images.length; index++) {
     img.alt = `Image ${index + 1}`;
     img.width = 300;
     img.height = 200;
-    
+
     // Set the first image as active
     if (index === 0) img.classList.add('active');
     
-    // img.addEventListener('click', function() {
-    //     redirect(index);
-    // });
+    // Add event listener to redirect on click
+    img.addEventListener('click', function() {
+        redirect(index);
+    });
+
     container.appendChild(img);
 }
 
-
-    // Function to change the image
+// Function to change the image
 function changeImage() {
     var imgs = document.querySelectorAll('.image-container img');
-    imgs[currentIndex].classList.remove('active'); 
-    currentIndex = (currentIndex + 1) % imgs.length; 
-    imgs[currentIndex].classList.add('active'); 
+    imgs[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % imgs.length;
+    imgs[currentIndex].classList.add('active');
 }
 
-    
 setInterval(changeImage, 3000);
